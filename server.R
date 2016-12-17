905267b283194e8aee635754f9495809cf843184
ShirinG <shirin.glander@gmail.com>
2016-12-17 16:54:28 +0100

    Finished app

--- server.R
+++ server.R
@@ -9,27 +9,21 @@ chr_data <- within(chr_data, SEQNAME <- factor(SEQNAME, levels = f))
 library(ggplot2)
 
 gwas38 <- read.table("gwas38.txt", header = TRUE, sep = "\t")
+gwas38_traits <- read.table("gwas38_traits.txt", header = TRUE, sep = "\t")
 
 # Define server logic required to plot variables
 shinyServer(function(input, output) {
 
   # Generate a plot of the requested variables
   output$plot <- renderPlot({
-    p <- ggplot(data = chr_data, aes(x = SEQNAME, y = as.numeric(SEQLENGTH))) +
-      geom_bar(stat = "identity", fill = "grey90", color = "black") +
-      theme(
-        axis.text = element_text(size = 14),
-        axis.title = element_text(size = 14),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_rect(fill = "white"),
-        legend.position = "bottom"
-      ) +
-      labs(x = "Chromosome", y = "Position")
 
     plot_snps <- function(trait){
 
-        trait = trait[-which(trait == "choose below")]
+      if (any(trait == "choose below")){
+        trait <- trait[-which(trait == "choose below")]
+      } else {
+        trait <- unique(trait)
+      }
 
         for (i in 1:length(unique(trait))){
 
@@ -42,6 +36,9 @@ shinyServer(function(input, output) {
                                       PVALUE_MLOG = snps_data_pre$PVALUE_MLOG,
                                       OR.or.BETA = snps_data_pre$OR.or.BETA)
 
+          snps_data_pre <- subset(snps_data_pre, PVALUE_MLOG > input$pvalmlog)
+          snps_data_pre <- subset(snps_data_pre, OR.or.BETA > input$orbeta | is.na(OR.or.BETA))
+
           if (i == 1){
 
             snps_data <- snps_data_pre
@@ -53,12 +50,42 @@ shinyServer(function(input, output) {
           }
         }
 
+      snps_data <- within(snps_data, Chr <- factor(Chr, levels = f))
+
+      p <- ggplot(data = snps_data, aes(x = Chr, y = as.numeric(Start))) +
+        geom_bar(data = chr_data, aes(x = SEQNAME, y = as.numeric(SEQLENGTH)), stat = "identity", fill = "grey90", color = "black") +
+        theme(
+          axis.text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill = "white"),
+          legend.position = "bottom"
+        ) +
+        labs(x = "Chromosome", y = "Position")
+
       p + geom_segment(data = snps_data, aes(x = as.numeric(as.character(Chr)) - 0.45, xend = as.numeric(as.character(Chr)) + 0.45,
                                            y = Start, yend = Start, colour = Trait), size = 2, alpha = 0.5) +
-        scale_colour_brewer(palette = "Set1")
+        scale_colour_brewer(palette = "Set1") +
+        guides(colour = guide_legend(ncol = 3, byrow = FALSE))
 
       }
 
-    print(plot_snps(trait = c(input$variable, input$variable2, input$variable3, input$variable4, input$variable5, input$variable6, input$variable7, input$variable8, input$variable9)))
+    plot_snps(trait = c(input$variable1, input$variable2, input$variable3, input$variable4, input$variable5, input$variable6, input$variable7, input$variable8, input$variable9))
   })
+
+  output$table <- renderTable({
+    table <- gwas38_traits[which(gwas38_traits$Trait %in% c(input$variable1, input$variable2, input$variable3, input$variable4, input$variable5, input$variable6, input$variable7, input$variable8)), ]
+
+    })
+
+  output$table2 <- renderTable({
+    table <- gwas38[which(gwas38$DISEASE.TRAIT %in% c(input$variable1, input$variable2, input$variable3, input$variable4, input$variable5, input$variable6, input$variable7, input$variable8)), c(1:5, 8, 10, 11, 13, 14, 20, 27, 32, 33, 34, 36)]
+
+    table <- subset(table, PVALUE_MLOG > input$pvalmlog)
+    table <- subset(table, OR.or.BETA > input$orbeta | is.na(OR.or.BETA))
+
+    table[order(table$seqnames), ]
+    })
+
 })
--- ui.R
+++ ui.R
@@ -3,46 +3,71 @@ library(shinythemes)
 
 gwas38_traits <- read.table("gwas38_traits.txt", header = TRUE, sep = "\t")
 
-diseases <- c("choose below", as.character(gwas38_traits$Var1))
+diseases <- c("choose below", as.character(gwas38_traits$Trait))
 
 shinyUI(fluidPage(theme = shinytheme("united"),
-                  titlePanel("GWAS disease-associated SNP locations in the human genome"),
+                  titlePanel("GWAS disease- & trait-associated SNP locations of the human genome"),
 
                   sidebarLayout(
                     sidebarPanel(
 
-                      selectInput("variable", "First disease:",
+                      sliderInput("pvalmlog",
+                                  "-log(p-value):",
+                                  min = -13,
+                                  max = 332,
+                                  value = -13),
+
+                      sliderInput("orbeta",
+                                  "odds ratio/ beta-coefficient :",
+                                  min = 0,
+                                  max = 4426,
+                                  value = 0),
+
+                      selectInput("variable1", "First trait:",
                                   choices = diseases[-1]),
 
-                      selectInput("variable2", "Second disease:",
+                      selectInput("variable2", "Second trait:",
                                   choices = diseases),
 
-                      selectInput("variable3", "Third disease:",
+                      selectInput("variable3", "Third trait:",
                                   choices = diseases),
 
-                      selectInput("variable4", "Fourth disease:",
+                      selectInput("variable4", "Fourth trait:",
                                   choices = diseases),
 
-                      selectInput("variable5", "Fifth disease:",
+                      selectInput("variable5", "Fifth trait:",
                                   choices = diseases),
 
-                      selectInput("variable6", "Sixth disease:",
+                      selectInput("variable6", "Sixth trait:",
                                   choices = diseases),
 
-                      selectInput("variable7", "Seventh disease:",
+                      selectInput("variable7", "Seventh trait:",
                                   choices = diseases),
 
-                      selectInput("variable8", "Eighth disease:",
-                                  choices = diseases),
-
-                      selectInput("variable9", "Ninth disease:",
+                      selectInput("variable8", "Eighth trait:",
                                   choices = diseases)
                     ),
                     mainPanel(
-                      div("Choose one or multiple diseases on the left-side panel und see their chromosomal locations below:", style = "color:blue"),
+                      br(),
+                      p("The National Human Genome Research Institute (NHGRI) catalog of Genome-Wide Association Studies (GWAS) is a curated resource of single-nucleotide polymorphisms (SNP)-trait associations. The database contains more than 100,000 SNPs and all SNP-trait associations with a p-value <1 × 10^−5."),
+                      p("This app is based on the 'gwascat' R package and its 'ebicat38' database and shows trait-associated SNP locations of the human genome."),
+                      p("For more info on how I built this app check out", a("my blog.", href = "https://shiring.github.io/")),
+                      br(),
+                      h4("How to use this app:"),
+                      div("Out of 1320 available traits or diseases you can choose up to 8 on the left-side panel und see their chromosomal locations below. The traits are sorted alphabetically. You can also start typing in the drop-down panel and traits matching your query will be suggested.", style = "color:blue"),
+                      br(),
+                      div("With the two sliders on the left-side panel you can select SNPs above a p-value threshold (-log of association p-value) and/or above an odds ratio/ beta-coefficient threshold. The higher the -log of the p-value the more significant the association of the SNP with the trait. Beware that some SNPs have no odds ratio value and will be shown regardless of the threshold.", style = "color:blue"),
+                      br(),
+                      div("The table directly below the plot shows the number of SNPs for each selected trait (without subsetting when p-value or odds ratio are changed). The second table below the first shows detailed information for each SNP of the chosen traits. This table shows only SNPs which are plotted (it subsets according to p-value and odds ratio thresholds).", style = "color:blue"),
+                      br(),
+                      div("Loading might take a few seconds...", style = "color:red"),
                       br(),
                       plotOutput("plot"),
-                      p("GWAS catalog:", a("http://www.ebi.ac.uk/gwas/", href = "http://www.ebi.ac.uk/gwas/"))
+                      tableOutput('table'),
+                      tableOutput('table2'),
+                      br(),
+                      p("GWAS catalog:", a("http://www.ebi.ac.uk/gwas/", href = "http://www.ebi.ac.uk/gwas/")),
+                      p(a("Welter, Danielle et al. “The NHGRI GWAS Catalog, a Curated Resource of SNP-Trait Associations.” Nucleic Acids Research 42.Database issue (2014): D1001–D1006. PMC. Web. 17 Dec. 2016.", href = "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3965119/"))
                     )
                   )
 ))
